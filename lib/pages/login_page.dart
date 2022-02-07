7bf255282378d325a1db28a0d10e1a6aa36432d8
MoizAsif <moiz.asif@xynotech.com>
2022-02-07 18:51:02 +0500

    Navigation and scroll view

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalogue/utils/routes.dart';
 
 // ignore: use_key_in_widget_constructors
 class LoginPage extends StatelessWidget {
@@ -6,50 +7,52 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_image.png",
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Welcome",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        hintText: "Enter username", labelText: "Username"),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        hintText: "Enter password", labelText: "Password"),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    child: const Text("Login"),
-                    style: TextButton.styleFrom(),
-                    onPressed: () {
-                      // ignore: avoid_print
-                      print("Hi ");
-                    },
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.cover,
               ),
-            )
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Welcome",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Enter username", labelText: "Username"),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          hintText: "Enter password", labelText: "Password"),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    ElevatedButton(
+                      child: const Text("Login"),
+                      style: TextButton.styleFrom(
+                          minimumSize: const Size(150, 40)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
