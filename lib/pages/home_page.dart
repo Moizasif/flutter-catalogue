fcd8fb5867cba534fc9e633f82120610b5d60b34
MoizAsif <moiz.asif@xynotech.com>
2022-02-24 15:23:52 +0500

    Local Files load and decode JSON

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,38 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalogue/models/catalog.dart';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
 // ignore: use_key_in_widget_constructors
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModal.items[0]);
     // const int days = 30;
     // const String name = "Codeup";
+
+    loadData() async {
+      final catalogJson =
+          await rootBundle.loadString("assets/files/catalog.json");
+      final decodedData = jsonDecode(catalogJson);
+      var productsData = decodedData["products"];
+      // ignore: avoid_print
+      print(productsData);
+    }
+
+    @override
+    void initState() {
+      super.initState();
+      loadData();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Catalogue App'),
